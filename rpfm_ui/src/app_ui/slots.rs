025e9c9afedaf7f9794a7b685b83310434cba515
Ismael Gutiérrez González <frodo_gv@hotmail.com>
2020-03-11 00:21:43 +0100

    One more for the credits.

--- rpfm_ui/src/app_ui/slots.rs
+++ rpfm_ui/src/app_ui/slots.rs
@@ -953,6 +953,7 @@ impl AppUISlots {
                         <li>LUA functions by: <b>Aexrael Dex</b>.</li>
                         <li>LUA Types for Kailua: <b>DrunkFlamingo</b>.</li>
                         <li>TW: Arena research and coding: <b>Trolldemorted</b>.</li>
+                        <li>Ca_vp8 research: <b>John Sirett</b>.</li>
                         <li>TreeView Icons made by <a href=\"https://www.flaticon.com/authors/smashicons\" title=\"Smashicons\">Smashicons</a> from <a href=\"https://www.flaticon.com/\" title=\"Flaticon\">www.flaticon.com</a>. Licensed under <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\" target=\"_blank\">CC 3.0 BY</a>
                     </ul>
 
