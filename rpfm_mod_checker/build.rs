852fe09c4b64b6306f27556995e471bc5b5acbb1
Frodo45127 <frodo_gv@hotmail.com>
2020-10-30 20:16:31 +0100

    Added icon to the mod checker.

---
+++ rpfm_mod_checker/build.rs
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2017-2020 Ismael Gutiérrez González. All rights reserved.
+//
+// This file is part of the Rusted PackFile Manager (RPFM) project,
+// which can be found here: https://github.com/Frodo45127/rpfm.
+//
+// This file is licensed under the MIT license, which can be found here:
+// https://github.com/Frodo45127/rpfm/blob/master/LICENSE.
+//---------------------------------------------------------------------------//
+
+/*!
+Build script for the RPFM Mod Checker.
+
+Here it goes all linking/cross-language compilation/platform-specific stuff that's needed in order to compile the RPFM Mod Checker.
+!*/
+
+/// This crate is only needed for the Windows Build.
+#[cfg(target_os = "windows")]
+use winres;
+
+/// Windows Build Script.
+#[cfg(target_os = "windows")]
+fn main() {
+
+    // Icon/Exe info gets added here.
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("./../img/rpfm.ico");
+    res.set("LegalCopyright","Copyright (c) 2017-2020 Ismael Gutiérrez González");
+    res.set("ProductName","Rusted PackFile Manager - Mod Checker Tool");
+    if let Err(error) = res.compile() { println!("Error: {}", error); }
+}
