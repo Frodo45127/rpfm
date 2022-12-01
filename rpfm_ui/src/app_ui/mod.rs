38dd6dc94896aaa3f65f10adb4e87a2914b733a2
Ismael Gutiérrez González <frodo_gv@hotmail.com>
2022-12-01 22:43:43 +0100

    Fixed warning.

--- rpfm_ui/src/app_ui/mod.rs
+++ rpfm_ui/src/app_ui/mod.rs
@@ -2422,20 +2422,25 @@ impl AppUI {
                         }
 
                         Response::AnimPackRFileInfo(container_info, files_info, file_info) => {
-                            PackedFileAnimPackView::new_view(&mut tab, app_ui, pack_file_contents_ui, container_info, &files_info);
+                            match PackedFileAnimPackView::new_view(&mut tab, app_ui, pack_file_contents_ui, container_info, &files_info) {
+                                Ok(_) => {
 
-                            // Add the file to the 'Currently open' list and make it visible.
-                            app_ui.tab_bar_packed_file.add_tab_3a(tab.get_mut_widget(), icon, &QString::from_std_str(""));
-                            app_ui.tab_bar_packed_file.set_current_widget(tab.get_mut_widget());
+                                    // Add the file to the 'Currently open' list and make it visible.
+                                    app_ui.tab_bar_packed_file.add_tab_3a(tab.get_mut_widget(), icon, &QString::from_std_str(""));
+                                    app_ui.tab_bar_packed_file.set_current_widget(tab.get_mut_widget());
 
-                            // Fix the tips view.
-                            let layout = tab.get_mut_widget().layout().static_downcast::<QGridLayout>();
-                            layout.add_widget_5a(tab.get_tips_widget(), 0, 99, layout.row_count(), 1);
+                                    // Fix the tips view.
+                                    let layout = tab.get_mut_widget().layout().static_downcast::<QGridLayout>();
+                                    layout.add_widget_5a(tab.get_tips_widget(), 0, 99, layout.row_count(), 1);
 
-                            let mut open_list = UI_STATE.set_open_packedfiles();
-                            open_list.push(tab);
-                            if data_source == DataSource::PackFile {
-                                pack_file_contents_ui.packfile_contents_tree_view().update_treeview(true, TreeViewOperation::UpdateTooltip(vec![file_info;1]), data_source);
+                                    let mut open_list = UI_STATE.set_open_packedfiles();
+                                    open_list.push(tab);
+                                    if data_source == DataSource::PackFile {
+                                        pack_file_contents_ui.packfile_contents_tree_view().update_treeview(true, TreeViewOperation::UpdateTooltip(vec![file_info;1]), data_source);
+                                    }
+                                },
+
+                                Err(error) => return show_dialog(&app_ui.main_window, error, false),
                             }
                         },
 
