574cc127c17adea058757d7b73fde262f74d17b9
Ismael Gutiérrez González <frodo_gv@hotmail.com>
2023-03-03 17:52:40 +0100

    Updated version number and schema default repo.

--- rpfm_ui/src/main.rs
+++ rpfm_ui/src/main.rs
@@ -260,7 +260,7 @@ lazy_static! {
 /// This constant gets RPFM's version from the `Cargo.toml` file, so we don't have to change it
 /// in two different places in every update.
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-const VERSION_SUBTITLE: &str = "I forgot about this message";
+const VERSION_SUBTITLE: &str = " -- 2.0 2: Electric Boogaloo";
 
 const MANUAL_URL: &str = "https://frodo45127.github.io/rpfm/";
 const GITHUB_URL: &str = "https://github.com/Frodo45127/rpfm";
