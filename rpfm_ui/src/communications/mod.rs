4be62f480576f7bd5514e2535a1aa072b88c6bcd
Frodo45127 <frodo_gv@hotmail.com>
2023-03-19 00:30:26 +0100

    Fixed incorrect feature.

--- rpfm_ui/src/communications/mod.rs
+++ rpfm_ui/src/communications/mod.rs
@@ -27,7 +27,7 @@ use rpfm_extensions::diagnostics::Diagnostics;
 use rpfm_extensions::search::{GlobalSearch, MatchHolder};
 
 use rpfm_lib::files::{anim_fragment::AnimFragment, anims_table::AnimsTable, audio::Audio, ContainerPath, video::SupportedFormats, db::DB, esf::ESF, image::Image, loc::Loc, matched_combat::MatchedCombat, pack::PackSettings, RFile, RFileDecoded, portrait_settings::PortraitSettings, text::Text, uic::UIC};
-#[cfg(feature = "enable_tools")] use rpfm_lib::files::rigidmodel::RigidModel;
+#[cfg(feature = "support_rigidmodel")] use rpfm_lib::files::rigidmodel::RigidModel;
 use rpfm_lib::games::pfh_file_type::PFHFileType;
 use rpfm_lib::integrations::{git::GitResponse, log::info};
 use rpfm_lib::notes::Note;
