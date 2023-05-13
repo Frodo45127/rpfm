cdfd42e3108ffbe86f0c0f18ff8d2c0311dcc0e5
Frodo45127 <frodo_gv@hotmail.com>
2023-05-14 00:53:01 +0200

    Moved matrix stuff to a Matrix trait, and cleanup.

--- rpfm_lib/src/files/bmd/battlefield_building_list/mod.rs
+++ rpfm_lib/src/files/bmd/battlefield_building_list/mod.rs
@@ -65,3 +65,90 @@ impl Encodeable for BattlefieldBuildingList {
         Ok(())
     }
 }
+
+impl ToLayer for BattlefieldBuildingList {
+    fn to_layer(&self) -> String {
+        let mut layer = String::new();
+
+        for building in self.buildings() {
+            layer.push_str(&format!("
+        <entity id=\"{:x}\">",
+                building.uid())
+            );
+
+            layer.push_str(&format!("
+            <ECBuilding
+                key=\"{}\"
+                damage=\"{}\"
+                indestructible=\"{}\"
+                toggleable=\"{}\"
+                key_building=\"{}\"
+                hide_tooltip=\"{}\"
+                settlement_level_configurable=\"{}\"
+                capture_location=\"\"
+                export_as_prop=\"false\"
+                visible_beyond_outfield=\"false\"/>",
+                building.building_key(),
+                building.properties().starting_damage_unary(),
+                building.properties().indestructible(),
+                building.properties().toggleable(),
+                building.properties().key_building(),
+                building.properties().hide_tooltip(),
+                building.properties().settlement_level_configurable(),
+            ));
+
+            layer.push_str(&format!("
+            <ECMeshRenderSettings
+                cast_shadow=\"{}\"
+                receive_decals=\"true\"
+                render_into_skydome_fog=\"false\"/>",
+                building.properties().cast_shadows()
+            ));
+
+            layer.push_str(&format!("
+            <ECVisibilitySettingsBattle visible_in_tactical_view=\"true\" visible_in_tactical_view_only=\"false\"/>"
+            ));
+
+            layer.push_str(&format!("
+            <ECWater is_water=\"false\"/>"
+            ));
+
+            let rotation_matrix = building.transform().rotation_matrix();
+            let scales = Transform3x4::extract_scales(rotation_matrix);
+            let normalized_rotation_matrix = Transform3x4::normalize_rotation_matrix(rotation_matrix, scales);
+            let angles= Transform3x4::rotation_matrix_to_euler_angles(normalized_rotation_matrix, true);
+
+            layer.push_str(&format!("
+            <ECTransform
+                position=\"{:.5} {:.5} {:.5}\"
+                rotation=\"{:.5} {:.5} {:.5}\"
+                scale=\"{:.5} {:.5} {:.5}\"
+                pivot=\"0 0 0\"/>",
+                building.transform().m30(), building.transform().m31(), building.transform().m32(),
+                angles.0, angles.1, angles.2,
+                scales.0, scales.1, scales.2
+            ));
+
+            layer.push_str(&format!("
+            <ECTerrainClamp
+                active=\"{}\"
+                clamp_to_sea_level=\"false\"
+                terrain_oriented=\"{}\"
+                fit_height_to_terrain=\"false\"/>",
+                *building.properties().clamp_to_surface() || building.height_mode() == "BHM_TERRAIN" || building.height_mode() == "BHM_TERRAIN_ALIGN_ORIENTATION",
+                building.height_mode() == "BHM_TERRAIN_ALIGN_ORIENTATION"
+            ));
+
+            layer.push_str(&format!("
+            <ECPrefabOverride enabled=\"false\" id=\"{}\"/>",
+                building.building_key())
+            );
+
+            layer.push_str("
+        </entity>"
+            );
+        }
+
+        layer
+    }
+}
--- rpfm_lib/src/files/bmd/common/mod.rs
+++ rpfm_lib/src/files/bmd/common/mod.rs
@@ -88,8 +88,9 @@ pub struct Rectangle {
     max_y: f32,
 }
 
-#[derive(Default, PartialEq, Clone, Debug, Getters, MutGetters, Setters, Serialize, Deserialize)]
-#[getset(get = "pub", get_mut = "pub", set = "pub")]
+// NOTE: These two don't have automatic getters. They get their getters from the Matrix trait.
+#[derive(Default, PartialEq, Clone, Debug, MutGetters, Setters, Serialize, Deserialize)]
+#[getset(get_mut = "pub", set = "pub")]
 pub struct Transform3x4{
     m00: f32,
     m01: f32,
@@ -105,8 +106,8 @@ pub struct Transform3x4{
     m32: f32,
 }
 
-#[derive(Default, PartialEq, Clone, Debug, Getters, MutGetters, Setters, Serialize, Deserialize)]
-#[getset(get = "pub", get_mut = "pub", set = "pub")]
+#[derive(Default, PartialEq, Clone, Debug, MutGetters, Setters, Serialize, Deserialize)]
+#[getset(get_mut = "pub", set = "pub")]
 pub struct Transform4x4 {
     m00: f32,
     m01: f32,
@@ -406,5 +407,217 @@ impl Encodeable for Transform4x4 {
         buffer.write_f32(self.m33)?;
 
         Ok(())
+    }
+}
+
+/// This trait abstract common behavior between the known transforms.
+pub trait Matrix {
+    fn m00(&self) -> f32;
+    fn m01(&self) -> f32;
+    fn m02(&self) -> f32;
+    fn m03(&self) -> f32;
+    fn m10(&self) -> f32;
+    fn m11(&self) -> f32;
+    fn m12(&self) -> f32;
+    fn m13(&self) -> f32;
+    fn m20(&self) -> f32;
+    fn m21(&self) -> f32;
+    fn m22(&self) -> f32;
+    fn m23(&self) -> f32;
+    fn m30(&self) -> f32;
+    fn m31(&self) -> f32;
+    fn m32(&self) -> f32;
+    fn m33(&self) -> f32;
+
+    /// Restores normal Rotation matrix representation as on the picture:
+    /// https://developer.unigine.com/forum/uploads/monthly_2020_05/image.png.674c8b961433f2a7a62c54bc55cb599c.png
+    /// pic note: (it seems like R should be applies to each column)
+    /// from CA's column-first serialization
+    fn rotation_matrix(&self) -> Matrix3<f64> {
+
+        // Fix order of the elements here
+        let matrix = Matrix3::new(
+            self.m00() as f64, self.m10() as f64, self.m20() as f64,
+            self.m01() as f64, self.m11() as f64, self.m21() as f64,
+            self.m02() as f64, self.m12() as f64, self.m22() as f64
+        );
+        matrix
+    }
+
+    ///Extracts scales as described here:
+    ///https://math.stackexchange.com/a/1463487
+    ///DOES NOT SUPPORT NEGATIVE SCALES
+    fn extract_scales(matrix: Matrix3<f64>) -> (f64, f64, f64) {
+        let scale = (
+            matrix.column(0).norm(),
+            matrix.column(1).norm(),
+            matrix.column(2).norm()
+        );
+        scale
+    }
+
+    fn apply_scales(matrix: Matrix3<f64>, scales: (f64, f64, f64)) -> Matrix3<f64> {
+        let scaled_matrix = Matrix3::new(
+            matrix[(0, 0)] * scales.0, matrix[(0, 1)] * scales.1, matrix[(0, 2)] * scales.2,
+            matrix[(1, 0)] * scales.0, matrix[(1, 1)] * scales.1, matrix[(1, 2)] * scales.2,
+            matrix[(2, 0)] * scales.0, matrix[(2, 1)] * scales.1, matrix[(2, 2)] * scales.2,
+        );
+        scaled_matrix
+    }
+
+    fn normalize_rotation_matrix(matrix: Matrix3<f64>, scales: (f64, f64, f64)) -> Matrix3<f64> {
+        let normalized_matrix = Matrix3::new(
+            matrix[(0, 0)] / scales.0, matrix[(0, 1)] / scales.1, matrix[(0, 2)] / scales.2,
+            matrix[(1, 0)] / scales.0, matrix[(1, 1)] / scales.1, matrix[(1, 2)] / scales.2,
+            matrix[(2, 0)] / scales.0, matrix[(2, 1)] / scales.1, matrix[(2, 2)] / scales.2,
+        );
+        normalized_matrix
+    }
+
+    /*
+    As I understand it uses 'xyz' extrinsic rotations order
+    Python code using scipy lib:
+        r = Rotation.from_euler("xyz", [-130.00000555832042, 80.00000457701574, -29.999991697018082], degrees=True)
+        m = r.as_matrix()
+        r = Rotation.from_matrix(m)
+        angles = r.as_euler("xyz", degrees=True)
+     */
+    fn rotation_matrix_to_euler_angles(matrix: Matrix3<f64>, degrees: bool) -> (f64, f64, f64) {
+        let rotation = Rotation3::from_matrix_unchecked(matrix);
+        let euler = rotation.euler_angles();
+        let euler_angles = if degrees {
+            (
+                euler.0.to_degrees(),
+                euler.1.to_degrees(),
+                euler.2.to_degrees(),
+            )
+        } else {
+           (euler.0, euler.1, euler.2)
+        };
+        euler_angles
+    }
+
+    fn euler_angles_to_rotation_matrix(angles: (f64, f64, f64), degrees: bool) -> Matrix3<f64> {
+        let _angles = if degrees {
+            (
+                angles.0.to_radians(),
+                angles.1.to_radians(),
+                angles.2.to_radians(),
+            )
+        } else {
+            angles
+        };
+        let rotation = Rotation3::from_euler_angles(_angles.0, _angles.1, _angles.2);
+        let mut matrix : Matrix3<f64> = rotation.into();
+
+        //prettify
+        matrix.iter_mut().for_each(|element| {
+            if element.abs() < 1e-5 {
+                *element = 0.0;
+            }
+        });
+        matrix
+    }
+}
+
+impl Matrix for Transform3x4 {
+    fn m00(&self) -> f32 {
+        self.m00
+    }
+    fn m01(&self) -> f32 {
+        self.m01
+    }
+    fn m02(&self) -> f32 {
+        self.m02
+    }
+    fn m03(&self) -> f32 {
+        0.0
+    }
+    fn m10(&self) -> f32 {
+        self.m10
+    }
+    fn m11(&self) -> f32 {
+        self.m11
+    }
+    fn m12(&self) -> f32 {
+        self.m12
+    }
+    fn m13(&self) -> f32 {
+        0.0
+    }
+    fn m20(&self) -> f32 {
+        self.m20
+    }
+    fn m21(&self) -> f32 {
+        self.m21
+    }
+    fn m22(&self) -> f32 {
+        self.m22
+    }
+    fn m23(&self) -> f32 {
+        0.0
+    }
+    fn m30(&self) -> f32 {
+        self.m30
+    }
+    fn m31(&self) -> f32 {
+        self.m31
+    }
+    fn m32(&self) -> f32 {
+        self.m32
+    }
+    fn m33(&self) -> f32 {
+        1.0
+    }
+}
+
+impl Matrix for Transform4x4 {
+    fn m00(&self) -> f32 {
+        self.m00
+    }
+    fn m01(&self) -> f32 {
+        self.m01
+    }
+    fn m02(&self) -> f32 {
+        self.m02
+    }
+    fn m03(&self) -> f32 {
+        self.m03
+    }
+    fn m10(&self) -> f32 {
+        self.m10
+    }
+    fn m11(&self) -> f32 {
+        self.m11
+    }
+    fn m12(&self) -> f32 {
+        self.m12
+    }
+    fn m13(&self) -> f32 {
+        self.m13
+    }
+    fn m20(&self) -> f32 {
+        self.m20
+    }
+    fn m21(&self) -> f32 {
+        self.m21
+    }
+    fn m22(&self) -> f32 {
+        self.m22
+    }
+    fn m23(&self) -> f32 {
+        self.m23
+    }
+    fn m30(&self) -> f32 {
+        self.m30
+    }
+    fn m31(&self) -> f32 {
+        self.m31
+    }
+    fn m32(&self) -> f32 {
+        self.m32
+    }
+    fn m33(&self) -> f32 {
+        self.m33
     }
 }
