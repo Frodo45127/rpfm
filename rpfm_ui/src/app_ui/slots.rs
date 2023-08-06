40e5af2386d2713366035c196639717c4e36f96b
Frodo45127 <frodo_gv@hotmail.com>
2023-08-06 20:08:20 +0200

    Fixed incorrect warning when generating the dependencies cache in empire and napoleon.

--- rpfm_ui/src/app_ui/slots.rs
+++ rpfm_ui/src/app_ui/slots.rs
@@ -982,7 +982,7 @@ impl AppUISlots {
                 if AppUI::are_you_sure_edition(&app_ui, "generate_dependencies_cache_are_you_sure") {
                     info!("Triggering `Generate Dependencies Cache` By Slot");
 
-                    if !setting_path(&format!("{}_assembly_kit", GAME_SELECTED.read().unwrap().key())).is_dir() ||
+                    if (GAME_SELECTED.read().unwrap().raw_db_version() > &0 && !setting_path(&format!("{}_assembly_kit", GAME_SELECTED.read().unwrap().key())).is_dir()) ||
                         (*GAME_SELECTED.read().unwrap().raw_db_version() == 0 && !old_ak_files_path().unwrap_or(PathBuf::new()).join(GAME_SELECTED.read().unwrap().key()).is_dir()) {
                         show_dialog(&app_ui.main_window, tr("generate_dependencies_cache_warn"), false);
                     }
