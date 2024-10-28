40251a86c10d3c1d8edf8c1cd370533637ab4b73
Frodo45127 <frodo_gv@hotmail.com>
2024-10-28 11:46:11 +0100

    Removed old debug.

--- rpfm_lib/src/schema/mod.rs
+++ rpfm_lib/src/schema/mod.rs
@@ -431,8 +431,6 @@ impl Schema {
                 definition.fields.iter_mut().for_each(|field| {
                     if let Some((ref_table, ref_column)) = field.is_reference(None) {
                         if ref_table.trim().is_empty() || ref_column.trim().is_empty() {
-                            dbg!(&table_name);
-                            dbg!(field.name());
                             field.is_reference = None;
                         }
                     }
