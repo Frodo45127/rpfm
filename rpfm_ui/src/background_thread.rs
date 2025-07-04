9dddb9fd6612e77e9a7a09d383985071c1f02fa9
Frodo45127 <frodo_gv@hotmail.com>
2025-07-04 14:03:29 +0200

    Fixed translator not autotranslating same-as-vanilla lines.

--- rpfm_ui/src/background_thread.rs
+++ rpfm_ui/src/background_thread.rs
@@ -2240,15 +2240,14 @@ pub fn background_loop() {
                                 let vanilla_loc_path = remote_path.join(format!("{}/{}", GAME_SELECTED.read().unwrap().key(), VANILLA_LOC_NAME));
                                 if let Ok(mut vanilla_loc) = RFile::tsv_import_from_path(&vanilla_loc_path, &None) {
                                     let _ = vanilla_loc.guess_file_type();
-
-                                    if let Ok(Some(RFileDecoded::Loc(vanilla_loc))) = vanilla_loc.decode(&None, true, false) {
+                                    if let Ok(RFileDecoded::Loc(vanilla_loc)) = vanilla_loc.decoded() {
 
                                         // If we have a fixes file for the vanilla translation, apply it before everything else.
                                         let fixes_loc_path = remote_path.join(format!("{}/{}{}.tsv", GAME_SELECTED.read().unwrap().key(), VANILLA_FIXES_NAME, language));
                                         if let Ok(mut fixes_loc) = RFile::tsv_import_from_path(&fixes_loc_path, &None) {
                                             let _ = fixes_loc.guess_file_type();
 
-                                            if let Ok(Some(RFileDecoded::Loc(fixes_loc))) = fixes_loc.decode(&None, false, true) {
+                                            if let Ok(RFileDecoded::Loc(fixes_loc)) = fixes_loc.decoded() {
                                                 base_english.extend(fixes_loc.data().iter().map(|x| (x[0].data_to_string().to_string(), x[1].data_to_string().to_string())).collect::<Vec<_>>());
                                             }
                                         }
