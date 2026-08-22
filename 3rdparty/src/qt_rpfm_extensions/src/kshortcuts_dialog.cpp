76c85cad771bf77a40115880ce775b0940b94e7f
Frodo45127 <frodo_gv@hotmail.com>
2026-08-22 14:30:22 +0200

    Changed default binding for copy files to "Ctrl+Shift+C".

--- 3rdparty/src/qt_rpfm_extensions/src/kshortcuts_dialog.cpp
+++ 3rdparty/src/qt_rpfm_extensions/src/kshortcuts_dialog.cpp
@@ -116,7 +116,7 @@ extern "C" void shortcut_collection_init(QWidget* parent, QList<QObject*>* short
     new_action(pack_tree_actions, "extract", "Extract", Qt::ShortcutContext::WidgetShortcut, QKeySequence::listFromString("Ctrl+E"), "archive-extract");
     new_action(pack_tree_actions, "rename", "Rename", Qt::ShortcutContext::WidgetShortcut, {QKeySequence("Ctrl+R"), QKeySequence("F2")}, "edit-move");
     new_action(pack_tree_actions, "copy_path", "Copy Path", Qt::ShortcutContext::WidgetShortcut, QKeySequence::listFromString(""), "edit-copy-path");
-    new_action(pack_tree_actions, "copy", "Copy", Qt::ShortcutContext::WidgetShortcut, QKeySequence::listFromString("Ctrl+C"), "edit-copy");
+    new_action(pack_tree_actions, "copy", "Copy", Qt::ShortcutContext::WidgetShortcut, QKeySequence::listFromString("Ctrl+Shift+C"), "edit-copy");
     new_action(pack_tree_actions, "cut", "Cut", Qt::ShortcutContext::WidgetShortcut, QKeySequence::listFromString("Ctrl+X"), "edit-cut");
     new_action(pack_tree_actions, "paste", "Paste", Qt::ShortcutContext::WidgetShortcut, QKeySequence::listFromString("Ctrl+V"), "edit-paste");
     new_action(pack_tree_actions, "duplicate", "Duplicate", Qt::ShortcutContext::WidgetShortcut, QKeySequence::listFromString("Ctrl+Shift+D"), "edit-copy");
